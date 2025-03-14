a554a9f260c91930ff2adaf243ea8cfff85917ea
0xrm1 <semabnb@gmail.com>
2025-03-14 22:41:32 +0300

    tailwind.config.js dosyası eklendi

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#c8ec64',
+        secondary: '#2a3a4f',
+        background: '#1b2839',
+      },
+    },
+  },
+  plugins: [],
+}
